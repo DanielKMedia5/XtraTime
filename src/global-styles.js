b86b3add79702790a557843295797115ea4997d8
DanielKMedia5 <101399881+DanielKMedia5@users.noreply.github.com>
2022-03-11 16:27:25 +0000

    Update global-styles.js

--- src/global-styles.js
+++ src/global-styles.js
@@ -2,7 +2,7 @@ import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyles = createGlobalStyle`
 html, body {
-    font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
+    font-family: 'Helvetica Neue', Helvetica, Trebuchet MS, Century Gothic, Candara, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     background-color: #EBEBEB;
