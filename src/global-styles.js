73f1a3a23d7e5fc7296b0d911f647e1e05ac8f0a
DanielKMedia5 <101399881+DanielKMedia5@users.noreply.github.com>
2022-06-07 14:31:03 +0100

    Add files via upload

--- src/global-styles.js
+++ src/global-styles.js
@@ -3,7 +3,7 @@ import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyles = createGlobalStyle`
 html, body {
-    font-family: 'Helvetica Neue Regular', Helvetica, Trebuchet MS, Century Gothic, Candara, sans-serif;
+    font-family: 'Helvetica Neue', Helvetica, Trebuchet MS, Century Gothic, Candara, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     background-color: #EBEBEB;
